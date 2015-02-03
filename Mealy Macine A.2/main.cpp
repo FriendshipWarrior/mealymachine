c328492e0e0e78e3b999f1101d6446760991e25b
Dustin Hurst <the_friendship_warrior@hotmail.com>
2015-02-03 13:50:04 -0700

    Initial Commit

---
+++ Mealy Macine A.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mealy Macine A.2
+//
+//  Created by Dustin Hurst on 2/3/15.
+//  Copyright (c) 2015 Dustin Hurst. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
